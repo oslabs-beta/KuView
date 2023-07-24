e8296503d10da28fc8d38738e3eff1de7e0f1215
daorichard <98173508+daorichard@users.noreply.github.com>
2023-07-24 16:04:58 -0400

    added message rendering for incorrect login and fake dashboard load

--- src/components/LoadingCube.jsx
+++ src/components/LoadingCube.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 const LoadingCube = () => {
   return (
-    <div className='container'>
-      <div className='cube'>
-        <div className='sides'>
-          <div className='top'></div>
-          <div className='right'></div>
-          <div className='bottom'></div>
-          <div className='left'></div>
-          <div className='front'></div>
-          <div className='back'></div>
+    <div className='wrapper'>
+      <div className='container'>
+        <div className='cube'>
+          <div className='sides'>
+            <div className='top'></div>
+            <div className='right'></div>
+            <div className='bottom'></div>
+            <div className='left'></div>
+            <div className='front'></div>
+            <div className='back'></div>
+          </div>
         </div>
+        <h2 className='text'>Loading Metrics...</h2>
       </div>
-      <div className='text'>KuView Loading Metrics...</div>
     </div>
   );
 };
