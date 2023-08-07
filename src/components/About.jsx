7d09a9e47e32af982af09af1599ebec02f9f039e
Ericesposito <eespo93@optonline.net>
2023-08-07 16:03:47 -0400

    made progress with about page, added gifs on cards, changed logo, ready for finishing touches on styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 import { useState } from 'react';
 import AboutCard from './AboutCard';
 
+import installgif from '../gifs/install.gif'
+import customizegif from '../gifs/customize.gif'
+import registergif from '../gifs/register.gif'
 function About(props) {
   const cardText = [
-    'Seamlessly and automatically installed on your computer by logging into the browser app',
-    'KuView provides you with a customized interactive dashboard to monitor your cluster',
-    'Utilize Prometheus and Grafana to scrape metric-monitoring data for your Minikube cluster',
-    'Built with Test-Driven Development in mind',
-    'Back end routes were built out using the Model-View-Controller design pattern using Node and Express',
-    'Data is securely stored via Bcrypt encryption'
+    ['Create a KuView account to monitor your Minikube cluster data', registergif],
+    ['Seamlessly and automatically installed on your computer by logging into the browser app', installgif],
+    ['KuView provides you with a customized interactive dashboard to monitor your cluster', customizegif]
   ];
 
   const boxes = [];
   for (let i = 0; i < cardText.length; i++) {
-    boxes.push(<AboutCard cardText={cardText[i]}/>);
+    let direction;
+    i % 2 === 0 ? direction = 'left' : direction = 'right'
+    boxes.push(<AboutCard cardText={cardText[i][0]} gif={cardText[i][1]} textLocation={direction} />);
   }
 
   return (
     <div className='about-page'>
       <div className='about-intro'>
-        <div>
-          {/* <h1>KuView</h1> */}
-        </div>
         <div>
           <h1>Monitor your Kubernetes Cluster, right here in your browser.</h1>
         </div>
@@ -30,7 +29,7 @@ function About(props) {
           Here at KuView, we have created an app for visualizing and providing real-time metric monitoring for your Kubernetes cluster.
         </div>
       </div>
-      <div className='about-grid'>
+      <div className='about-boxes'>
         {boxes}
       </div>
     </div>
--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 const AboutCard = (props) => {
   console.log('prop in AboutCard', props);
-
-
+  const gif = <img src={props.gif} className='gifImgTag'/>
+  const text = <p>{props.cardText}</p>
+  let name = {}
+  if(props.textLocation === 'left'){
+    name.textAlign = 'right'
+  }else{
+    name.textAlign = 'left'
+  }
   return (
     <div className='about-card-div'>
-      {props.cardText}
+      <div style={name}>
+        {props.textLocation === 'left' ? text : gif}
+      </div>
+      <div style={name}>
+        {props.textLocation === 'left' ? gif : text}
+      </div>
     </div>
   );
 };
