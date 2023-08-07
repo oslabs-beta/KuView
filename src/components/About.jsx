e2c64fa80a2b9d2ee71517f7ecb1787e721dfcc4
Ericesposito <eespo93@optonline.net>
2023-08-07 09:15:54 -0400

    working on about page progress

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
+import AboutCard from './AboutCard';
 
-function About() {
+function About(props) {
+  const cardText = [
+    'Seamlessly and automatically installed on your computer by logging into the browser app',
+    'KuView provides you with a customized interactive dashboard to monitor your cluster',
+    'Utilize Prometheus and Grafana to scrape metric-monitoring data for your Minikube cluster',
+    'Built with Test-Driven Development in mind',
+    'Back end routes were built out using the Model-View-Controller design pattern using Node and Express',
+    'Data is securely stored via Bcrypt encryption'
+  ];
+
+  const boxes = [];
+  for (let i = 0; i < cardText.length; i++) {
+    boxes.push(<AboutCard cardText={cardText[i]}/>);
+  }
+
   return (
-    <div>
-      About
+    <div className='about-page'>
+      <div className='about-intro'>
+        <div>
+          {/* <h1>KuView</h1> */}
+        </div>
+        <div>
+          <h1>Monitor your Kubernetes Cluster, right here in your browser.</h1>
+        </div>
+        <div>
+          Here at KuView, we have created an app for visualizing and providing real-time metric monitoring for your Kubernetes cluster.
+        </div>
+      </div>
+      <div className='about-grid'>
+        {boxes}
+      </div>
     </div>
+    
   );
 }
 
--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 const AboutCard = (props) => {
   console.log('prop in AboutCard', props);
+
+
   return (
-    <div className='iframe-container'>
-      <iframe
-        title='myIframe'
-        src={`http://localhost:3000/d/${props.cookie}/kuview-tech-demo?orgId=1&refresh=5s`}
-        key='fullDashboard'
-      ></iframe>
+    <div className='about-card-div'>
+      {props.cardText}
     </div>
   );
 };
 
-export default Iframe;
+export default AboutCard;
